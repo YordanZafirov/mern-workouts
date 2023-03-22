8830d881f38cb3b8b1bb0162353a01b1b1642c6b
YordanZafirov <iordanzafirov17@gmail.com>
2023-03-22 17:54:10 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const workoutRoutes = require('./routes/workouts');
+
+const port = process.env.PORT || 4000
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database');
+    // listen to port
+    app.listen(port, () => {
+      console.log('listening on port', port);
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+  }) 
